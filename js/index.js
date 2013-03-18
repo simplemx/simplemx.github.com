3e2b292473dd4d06ba7ec4b29b128be14a16ac73
simplemx <simplemx@126.com>
2013-03-18 21:48:31 +0800

    change css

--- js/index.js
+++ js/index.js
@@ -2,7 +2,5 @@ $(function(){
 	var blogs = blog_list;
 	var templateData = $("#indexTemplate").html();
 	var templateHtml = Mustache.to_html(templateData, { blogs: blogs });
-	var content = $("#content");
-	content.empty();
-	content.append(templateHtml);
+	$("body").append(templateHtml);
 });
