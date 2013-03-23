fe845b193b3a0771a131b42471b9eb3463576887
simplemx <simplemx@126.com>
2013-03-23 09:40:41 +0800

    add blog date

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 $(function(){
 	var blogs = blog_list;
+	var str = null;
+	for(var i = 0; i < blog_list.length; i++){
+		str = blogs[i]["file"]; 
+		blogs[i]["date"] = str.substring(0, str.indexOf("."));
+	}
 	var templateData = $("#indexTemplate").html();
 	var templateHtml = Mustache.to_html(templateData, { blogs: blogs });
 	$("body").append(templateHtml);
