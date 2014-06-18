13dac7f145c5c7780b22576190fe0572a02b8085
unknown <simplemx@126.com>
2014-06-18 09:35:51 +0800

    add blog pages

--- js/index.js
+++ js/index.js
@@ -1,11 +1,46 @@
 $(function(){
-	var blogs = blog_list;
-	var str = null;
-	for(var i = 0; i < blog_list.length; i++){
-		str = blogs[i]["file"]; 
-		blogs[i]["date"] = str.substring(0, str.indexOf("."));
-	}
+	var blogs = blog_list,
+	    str = null,
+        page_size = 10,
+        page_index = 0;
+    
+	$.each(blogs, function(i, blog){
+		str = blog["file"]; 
+		blog["date"] = str.substring(0, str.indexOf("."));
+	});
+    
+    var show_next_page = function(){
+        if (page_index == blogs.length) {
+            $win.unbind("scroll");
+            return;
+        }
+
+        var show_blogs = [],
+            len = (page_index + page_size) > blogs.length ? blogs.length : (page_index + page_size);
+        for (var i = page_index; i < len; i++) {
+            show_blogs.push(blogs[i]);
+        }
+
+        page_index = len;
+
+        var templateData = $("#blogsTemplate").html();
+	    var templateHtml = Mustache.to_html(templateData, { blogs: show_blogs });
+	    $("#main_content").append(templateHtml);
+    };
+
+/*
 	var templateData = $("#blogsTemplate").html();
 	var templateHtml = Mustache.to_html(templateData, { blogs: blogs });
 	$("#main_content").append(templateHtml);
+    */
+    show_next_page();
+    
+    var $win = $(window);
+    var $document = $(document);
+    $win.scroll(function(){
+        if ($win.height() + $win.scrollTop()  == $document.height()) {
+            show_next_page()
+        }
+        
+    })
 });
